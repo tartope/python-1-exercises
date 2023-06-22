e2eea1a2b97e71c3ae609e16be88a2d09be4279a
Tunisia Artope <tartope@xpanxion.com>
2023-06-22 14:27:03 -0400

    Complete exercise 1.

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -1,2 +1,7 @@
-def ex1():
-    print("who's there")
+# def ex1():
+#     print("who's there")
+
+def hello_world(string):
+    num = int(string)
+    for i in range(num):
+        print("Hello World from Python!")
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from exercises import ex1
 
 
 def main():
-    print('ding dong')
-    ex1.ex1()
+    # print('ding dong')
+    # ex1.ex1()
+    ex1.hello_world(3)
 
 
 if __name__ == '__main__':
