89f2e01c62f74fe0dbf2ddc6c57cee991c6039b4
Tunisia Artope <tartope@xpanxion.com>
2023-06-22 16:48:22 -0400

    Complete exercise 2.

---
+++ exercises/ex2.py
@@ -0,0 +1,5 @@
+def array_to_string(arr):
+    result = ""
+    for ele in arr:
+        result = result + " " + str(ele)
+    print(result)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from exercises import ex1
+from exercises import ex1, ex2
 
 
 def main():
     # print('ding dong')
     # ex1.ex1()
-    ex1.hello_world(3)
+    # ex1.hello_world(3)
+    ex2.array_to_string([1,2,3])
 
 
 if __name__ == '__main__':
